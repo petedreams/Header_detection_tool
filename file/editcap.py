cf363c7302a98b343e5e8a46e96b48f9bbd73553
takashi <ttt@ttt.com>
2015-01-30 01:07:25 +0900

    add

---
+++ file/editcap.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#20150128
+#editcap.py
+#pcap‚ğŠÔ•ªŠ„
+#g‚¢•û ./editcap.py <infile> <outfile> <sec>
+
+import os,sys,dpkt,datetime,time
+
+def writefilepath(d,outfile,counter):
+    timestr=d.strftime("%Y%m%d%H%M%S")
+    path, outfilename = os.path.split(outfile)
+    name, ext = os.path.splitext(outfilename)
+    writefile=path+'/'+name+'_'+str(counter).zfill(5)+'_'+timestr+ext
+    return writefile
+
+def editcap(infile,outfile,sp):
+#pcap“Ç‚İ‚İ
+    s_time=None#Šî€ŠÔ
+    counter=0
+    pcount=1
+
+    f= open(infile)
+    pcap = dpkt.pcap.Reader(f)
+    for ts,buf in pcap:
+        writedflag=True
+        try:
+            eth = dpkt.ethernet.Ethernet(buf)
+        except:
+            continue
+        if not s_time:
+            init_d = datetime.datetime.fromtimestamp(int(ts))#mirosecondØ‚èÌ‚Ä
+            writefile = writefilepath(init_d,outfile,counter)
+            f=open(writefile,'wb')
+            writefile = dpkt.pcap.Writer(open(writefile,'wb'))
+            writefile.writepkt(eth,ts)
+            s_time = int(ts)
+            g_time = s_time+sp
+            counter+=1
+        elif ts < g_time:
+            pass
+            init_d = datetime.datetime.fromtimestamp(int(ts))#mirosecondØ‚èÌ‚Ä
+            writefile.writepkt(eth,ts)
+        elif g_time <= ts:
+            if ts < g_time+sp:
+                d = datetime.datetime.fromtimestamp(g_time)
+                writefile = writefilepath(d,outfile,counter)
+                writefile = dpkt.pcap.Writer(open(writefile,'wb'))
+                writefile.writepkt(eth,ts)
+                g_time += sp
+                counter+=1
+            else:
+                while writedflag:
+                    d = datetime.datetime.fromtimestamp(g_time)
+                    writefile = writefilepath(d,outfile,counter)
+                    writefile = dpkt.pcap.Writer(open(writefile,'wb'))
+                    g_time+=sp
+                    counter+=1
+                    if g_time+sp <= ts:
+                        continue
+                    else:
+                        d = datetime.datetime.fromtimestamp(g_time)
+                        writefile = writefilepath(d,outfile,counter)
+                        writefile = dpkt.pcap.Writer(open(writefile,'wb'))
+                        g_time+=sp
+                        counter+=1
+                        init_d = datetime.datetime.fromtimestamp(int(ts))#mirosecondØ‚èÌ‚Ä
+                        writefile.writepkt(eth,ts)
+                        writedflag=False
+        pcount+=1
+
+if __name__ == '__main__':
+    try:
+        infile = sys.argv[1]
+        outfile = sys.argv[2]
+        sp = int(sys.argv[3])
+        editcap(infile,outfile,sp)
+    except IOError:
+        print '[IOError] usage: editcap.py <infile> <outfile> <sec>'
+
